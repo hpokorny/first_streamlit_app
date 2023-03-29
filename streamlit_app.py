344072c18e1948ed8936a0cb3b0f5580995e6796
hpokorny <67473879+hpokorny@users.noreply.github.com>
2023-03-29 11:54:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,21 +52,24 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List:'):
+if streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
    
-  #don't run anything past here while we troubleshoot
-streamlit.stop()
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('Thanks for adding ', add_my_fruit)
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+def insert_row_snowflake(new_fruit):
+   with my_cnx_cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+   
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function_
+                  
 
-#illustrates that we have an issue with Control of Flow
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
